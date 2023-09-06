9b4f06bec29ebaecd3cdd94607380134d4abb8a5
jsvenker <123997897+jsvenker@users.noreply.github.com>
2023-09-06 17:20:40 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,59 +1,49 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import Timer, ClockCycles
+from cocotb.triggers import ClockCycles
 
 @cocotb.test()
 async def test_traffic_controller(dut):
     dut._log.info("Starting tt_um_traffic_controller_4way test")
-    clock = Clock(dut.clk, 1, units="us")  # Using a 1MHz clock
+    
+    # Parameters
+    MAX_COUNT = int(dut.MAX_COUNT.value)
+    GREEN_DURATION = 3 * MAX_COUNT
+    YELLOW_DURATION = (MAX_COUNT * 3) // 10
+    
+    clock = Clock(dut.clk, 1, units="us")  # Assuming a 1MHz clock for simplicity
     cocotb.start_soon(clock.start())
 
-    # Reset
+    # Reset sequence
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
-    
-    RED = 1
-    GREEN = 1
-    YELLOW = 1
-    
-    # Let's add a delay here to allow the traffic controller to stabilize after reset
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 10)  # Delay for stabilization
 
-    # Define a helper function to check the light outputs
     async def check_lights(direction, red, green, yellow):
         """Check the lights for a given direction."""
-        dut._log.info(f"uo_out: {dut.uo_out.value}, uio_out: {dut.uio_out.value}, direction: {direction}, Expected Green: {green}, Expected Yellow: {yellow}")
         assert dut.uo_out[direction*2 + 1] == red
         assert dut.uo_out[direction*2] == green
         assert dut.uio_out[direction*2 + 1] == yellow
 
-    # For each direction, ensure lights go through the correct sequence of GREEN -> YELLOW -> RED
     for direction in range(4):
         dut._log.info(f"Testing direction {direction}")
 
         # Set the request for the current direction
         dut.ui_in.value = 1 << direction
 
-        # Wait for a few cycles to ensure the light is stable
-        await ClockCycles(dut.clk, 5)
-
         # Check for GREEN
         await check_lights(direction, red=0, green=1, yellow=0)
 
-        # Wait for a few cycles before transitioning to YELLOW
-        await ClockCycles(dut.clk, 5)
-
-        # Check for YELLOW 
+        # Wait for GREEN_DURATION and then check for YELLOW
+        await ClockCycles(dut.clk, GREEN_DURATION)
         await check_lights(direction, red=0, green=0, yellow=1)
 
-        # Wait for a few cycles before transitioning to RED
-        await ClockCycles(dut.clk, 5)
-
-        # Check for RED
+        # Wait for YELLOW_DURATION, then check for RED
+        await ClockCycles(dut.clk, YELLOW_DURATION)
         await check_lights(direction, red=1, green=0, yellow=0)
 
-     # Reset
+    # Reset sequence
     dut.rst_n.value = 0
     await ClockCycles(dut.clk, 10)
     dut.rst_n.value = 1
