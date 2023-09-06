afea2f0b842f8a3130cad6beb646744044f50217
jsvenker <123997897+jsvenker@users.noreply.github.com>
2023-09-06 15:01:33 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -10,16 +10,8 @@ async def test_traffic_controller(dut):
 
     # Reset
     dut.rst_n.value = 0
-    await ClockCycles(dut.clk, 10)
+    await ClockCycles(dut.clk, 1)
     dut.rst_n.value = 1
-
-    # Since we're not able to retrieve MAX_COUNT from the DUT, let's use the hardcoded value
-    MAX_COUNT = 10_000_000
-    
-    # Defining the count values based on the hardcoded MAX_COUNT (10 seconds)
-    GREEN_DURATION_CYCLES = 30 * MAX_COUNT  # 30 seconds in clock cycles
-    YELLOW_DURATION_CYCLES = (3 * MAX_COUNT) / 10  # 3 seconds in clock cycles
-    RED_DURATION_CYCLES = MAX_COUNT - GREEN_DURATION_CYCLES - YELLOW_DURATION_CYCLES  # Remaining time
 
     # Define a helper function to check the light outputs
     async def check_lights(direction, red, green):
@@ -35,16 +27,22 @@ async def test_traffic_controller(dut):
         # Set the request for the current direction
         dut.ui_in.value = 1 << direction
 
+        # Wait for a few cycles to ensure the light is stable
+        await ClockCycles(dut.clk, 5)
+
         # Check for GREEN
-        await ClockCycles(dut.clk, int(GREEN_DURATION_CYCLES))
         await check_lights(direction, red=0, green=1)
 
-        # Check for YELLOW
-        await ClockCycles(dut.clk, int(YELLOW_DURATION_CYCLES))
-        await check_lights(direction, red=0, green=0)  # As per the design, yellow is indicated by both red and green being off
+        # Wait for a few cycles before transitioning to YELLOW
+        await ClockCycles(dut.clk, 5)
+
+        # Check for YELLOW (both red and green are off as per design)
+        await check_lights(direction, red=0, green=0)
+
+        # Wait for a few cycles before transitioning to RED
+        await ClockCycles(dut.clk, 5)
 
         # Check for RED
-        await ClockCycles(dut.clk, int(RED_DURATION_CYCLES))
         await check_lights(direction, red=1, green=0)
 
      # Reset
