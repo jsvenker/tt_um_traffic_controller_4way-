c2f4ea2379e89194c0281215f8462d9723bb22c8
jsvenker <123997897+jsvenker@users.noreply.github.com>
2023-09-06 23:33:11 -0500

    Update test.py

--- src/test.py
+++ src/test.py
@@ -1,41 +1,37 @@
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, FallingEdge, Timer, ClockCycles
+from cocotb.triggers import RisingEdge, Timer, ClockCycles
 from cocotb.binary import BinaryValue
 
 async def reset_dut(rst_n, clk):
     """Reset DUT."""
-    rst_n <= 0
+    rst_n.value = 0
     for _ in range(2):
         await RisingEdge(clk)
-    rst_n <= 1
+    rst_n.value = 1
     await RisingEdge(clk)
 
 @cocotb.test()
 async def test_traffic_controller_4way(dut):
     dut._log.info("Creating clock")
     clock = Clock(dut.clk, 1, units="us")
-    cocotb.fork(clock.start())
+    cocotb.start_soon(clock.start())
 
     # Reset DUT
     await reset_dut(dut.rst_n, dut.clk)
     
-    dut.ena <= 1  # Enable the traffic controller
+    dut.ena.value = 1  # Enable the traffic controller
 
     # Testing different input patterns for ui_in
-    # These patterns will change the current_direction and simulate the flow through different lights.
     input_patterns = [0x01, 0x02, 0x04, 0x08]
 
     for pattern in input_patterns:
-        dut.ui_in <= BinaryValue(pattern)
+        dut.ui_in.value = BinaryValue(pattern, n_bits=8)  # Ensure 8-bit assignment
         await ClockCycles(dut.clk, 5)
-        dut._log.info(f"ui_in={pattern}, uo_out={dut.uo_out.value}, uio_out={dut.uio_out.value}")
-
-        # Additional test logic and assertions can be added here based on expected outputs.
+        dut._log.info(f"ui_in={pattern:08b}, uo_out={dut.uo_out.value}, uio_out={dut.uio_out.value}")
 
     # Disable the traffic controller
-    dut.ena <= 0
+    dut.ena.value = 0
     await ClockCycles(dut.clk, 5)
 
     dut._log.info("Test complete")
-
